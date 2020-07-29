5f1dc47e63846d146a34529f19bf44af3d7b2b6f
Anthony Yang <ay339@cornell.edu>
2020-07-29 16:50:10 -0400

    Implement gui

--- zoomcc.py
+++ zoomcc.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import requests
+import tkinter as tk
+from tkinter import font as tkfont
 
 
 def post_session(url: str) -> str:
@@ -37,7 +39,13 @@ def start(name: str, zoom_api_token: str):
             print("Something odd happened with your speech")
 
 
-if __name__ == "__main__":
+def handle_initiate_click(input_name: str, input_token: str):
+    name = input_name.strip()
+    token = input_token.strip()
+    start(name, token)
+
+
+def cli():
     print("Input your display name:")
     name_input = input(">>").strip()
     if name_input == 'cancel':
@@ -49,3 +57,25 @@ if __name__ == "__main__":
     if usr_input == 'cancel':
         exit(0)
     start(name_input, usr_input)
+
+
+if __name__ == "__main__":
+    window = tk.Tk()
+
+    greeting = tk.Label(text="ZoomCC")
+    greeting.pack()
+
+    input_name_label = tk.Label(text="Input your display name")
+    input_name_label.pack()
+    input_name_entry = tk.Entry(fg="yellow", bg="blue", width=50)
+    input_name_entry.pack()
+
+    input_token_label = tk.Label(text="Input your token API from Zoom")
+    input_token_label.pack()
+    input_token_entry = tk.Entry(fg="yellow", bg="blue", width=50)
+    input_token_entry.pack()
+
+    enter_button = tk.Button(text="Initiate")
+    enter_button.pack()
+    enter_button.bind("<Button-1>", lambda: handle_initiate_click(input_name_entry.get(), input_token_entry.get()))
+    window.mainloop()
